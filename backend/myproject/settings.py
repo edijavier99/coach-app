e4fb927fee2d3930b5db817fdf70c65448cdaffc
edijavier99 <126507385+edijavier99@users.noreply.github.com>
2024-07-06 00:59:22 +0100

    login and auth and admin working properly now

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -163,16 +163,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #CORS AUTHORITZATION
 CORS_ALLOWED_ORIGINS = []
 
+AUTH_USER_MODEL = 'myapp.User'
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
 
-
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=10),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
