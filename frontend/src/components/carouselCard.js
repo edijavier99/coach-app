291d2fde23b89c280511ee10799134da9751e80f
edijavier99 <126507385+edijavier99@users.noreply.github.com>
2024-07-28 19:38:31 +0100

    the image of the first item of the carousel it is been moved 65% to the left

--- frontend/src/components/carouselCard.js
+++ frontend/src/components/carouselCard.js
@@ -4,7 +4,7 @@ import { BookingButton } from "./bookingButton";
 export const CarouselItem = ({ image, title, sloganDescription, slogan, active, customClass }) => {
   return (
     <div className={`carousel-item ${active ? 'active' : ''} ${customClass}`} data-bs-interval="5000" >
-      <img src={image} className="d-block w-100" alt={title} />
+      <img src={image} className="d-block w-100 carousel-image" alt={title} />
       <div className="carousel-caption ">
         <div
           className="d-flex flex-column  "
