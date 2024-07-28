2322b6117e49688de6541226aca9e5a8416478b1
edijavier99 <126507385+edijavier99@users.noreply.github.com>
2024-07-28 17:13:43 +0100

    blog / article model updated with second img

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -9,14 +9,16 @@ class Post(models.Model):
         ('mindset', 'Mindset'),
     ]
 
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique=True)  # Ensuring the slug is unique
-    subtitle = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)  # Ensuring the slug is unique
+    subtitle = models.TextField()
     description = models.TextField()
     day_posted = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    image_url = models.URLField(max_length=300, null=True, blank=True)  # Permitir que sea nulo y sin valor por defecto
+    image_url = models.URLField(max_length=350, null=True, blank=True)  # Permitir que sea nulo y sin valor por defecto
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES , null=True)
+    image_url_tow = models.URLField(max_length=350, null = True, blank = True)
+    second_paragraph = models.TextField(null = True, blank = True)
 
     def __str__(self):
         return self.title
